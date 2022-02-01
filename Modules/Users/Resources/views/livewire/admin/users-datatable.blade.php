bac4227948482fb3ba673a2ad16b523548f28d36
Goldoni <fotsa.goldoni@yahoo.fr>
2022-02-01 01:10:03 +0100

    initial init

--- Modules/Users/Resources/views/livewire/admin/users-datatable.blade.php
+++ Modules/Users/Resources/views/livewire/admin/users-datatable.blade.php
@@ -162,11 +162,16 @@
                             </x-table.cell>
 
                             <x-table.cell>
-                                <span class="text-gray-900 font-medium">{{ $item->created_at->format('d M Y H:m') }} </span>
+                                <span class="text-gray-900 font-medium truncate">{{ $item->created_at->format('d M Y H:m') }} </span>
                             </x-table.cell>
 
                             <x-table.cell>
-                                <x-button.link wire:click="edit({{ $item->id }})">Edit</x-button.link>
+                                <x-button.link wire:click="edit({{ $item->id }})" class="hover:text-gray-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                    </svg>
+                                </x-button.link>
                             </x-table.cell>
                         </x-table.row>
                     @empty
